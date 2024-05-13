0c8f7aaa254243fd42829bda39d2a690eb460663
Casey Mershon <caseymershon@gmail.com>
2024-05-13 15:18:53 -0400

    init creator module

--- apps/api/src/creator/creator.module.ts
+++ apps/api/src/creator/creator.module.ts
@@ -3,10 +3,11 @@ import { CreatorController } from './controllers/creator.controller';
 import { CreatorService } from './services/creator.service';
 import { RequestService } from './request/services/request.service';
 import { RequestModule } from './request/request.module';
+import { EmailModule } from '../email/email.module';
 
 @Module({
   controllers: [CreatorController],
   providers: [CreatorService, RequestService],
-  imports: [RequestModule]
+  imports: [RequestModule, EmailModule]
 })
 export class CreatorModule {}
