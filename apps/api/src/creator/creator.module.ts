bb09a3864b5fd750b3c56e2602baf3b30f47671b
Casey Mershon <caseymershon@gmail.com>
2024-05-16 15:55:59 -0400

    init creator profile module

--- apps/api/src/creator/creator.module.ts
+++ apps/api/src/creator/creator.module.ts
@@ -5,10 +5,12 @@ import { RequestService } from './request/services/request.service';
 import { RequestModule } from './request/request.module';
 import { EmailModule } from '../email/email.module';
 import { PersistenceModule } from '../persistence/persistence.module';
+import { ProfileController } from './profile/controllers/profile.controller';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  controllers: [CreatorController],
+  controllers: [CreatorController, ProfileController],
   providers: [CreatorService, RequestService],
-  imports: [RequestModule, EmailModule, PersistenceModule]
+  imports: [RequestModule, EmailModule, PersistenceModule, ProfileModule]
 })
 export class CreatorModule {}
