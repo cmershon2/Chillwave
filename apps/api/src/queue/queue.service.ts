d57518443a0a7597823792efc8879b35fd74d892
Casey Mershon <caseymershon@gmail.com>
2024-06-03 12:29:36 -0400

    implement queue service

--- apps/api/src/queue/queue.service.ts
+++ apps/api/src/queue/queue.service.ts
@@ -1,4 +1,12 @@
+import { InjectQueue } from '@nestjs/bull';
 import { Injectable } from '@nestjs/common';
+import { Queue } from 'bull';
 
 @Injectable()
-export class QueueService {}
+export class QueueService {
+    constructor(@InjectQueue('{video-upload}') private readonly videoUploadQueue: Queue) {}
+
+    async enqueueVideoUpload(videoData: any) {
+        await this.videoUploadQueue.add(videoData);
+    }
+}
