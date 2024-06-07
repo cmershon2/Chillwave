0064e9e7f3aa3736dc8bb5a4ab45a75507b7fbd1
Casey Mershon <caseymershon@gmail.com>
2024-06-07 13:29:15 -0400

    create S3 upload service

---
+++ apps/api/src/upload/s3-upload/s3-upload.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import * as AWS from 'aws-sdk';
+import { PassThrough } from 'stream';
+
+@Injectable()
+export class S3Service {
+  private s3: AWS.S3;
+
+  constructor() {
+    this.s3 = new AWS.S3({
+        s3ForcePathStyle: false,
+        endpoint: 'https://nyc3.digitaloceanspaces.com',
+        region: process.env.S3_REGION,
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID,
+          secretAccessKey: process.env.S3_ACCESS_KEY_SECRET,
+        },
+    });
+  }
+
+  async uploadFile(
+    file: Buffer,
+    bucketName: string,
+    objectKey: string,
+    contentType: string,
+  ): Promise<string> {
+    const pass = new PassThrough();
+    const params = {
+      Bucket: bucketName,
+      Key: objectKey,
+      Body: pass,
+      ContentType: contentType,
+    };
+
+    try {
+      pass.end(file);
+      const uploadResult = await this.s3.upload(params).promise();
+      return uploadResult.Location;
+    } catch (err) {
+      console.error('Error uploading file to S3:', err);
+      throw err;
+    }
+  }
+}
