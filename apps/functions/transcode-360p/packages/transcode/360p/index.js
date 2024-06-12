eb26f2d85cf1d659ecb4125e2ebae3bce103d402
Casey Mershon <caseymershon@gmail.com>
2024-06-11 20:41:10 -0400

    serverless transcoding experimentation

--- apps/functions/transcode-360p/packages/transcode/360p/index.js
+++ apps/functions/transcode-360p/packages/transcode/360p/index.js
@@ -1,51 +1,111 @@
-const Boto3 = require('aws-sdk/clients/s3');
-const FFmpeg = require('fluent-ffmpeg');
+const S3 = require('aws-sdk/clients/s3');
+const fs = require('fs');
+const path = require('path');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
 
-const s3 = new Boto3({
-  endpoint: process.env.S3_ENDPOINT,
-  accessKeyId: process.env.S3_ACCESS_KEY_ID,
-  secretAccessKey: process.env.S3_ACCESS_KEY_SECRET,
+const s3 = new S3({
+  s3ForcePathStyle: false,
+  endpoint: 'https://nyc3.digitaloceanspaces.com',
+  region: process.env.S3_REGION,
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_ACCESS_KEY_SECRET,
+  }
 });
 
-const BUCKET_NAME = 'chillwave';
-const OUTPUT_PREFIX = 'transcoded/360p/';
+exports.main = async (args) => {
+  const id = args.jobId;
 
-exports.main = async (event, context) => {
-  const { videoChunk, chunkIndex } = event.body;
+  if (!process.env.S3_ACCESS_KEY_ID || !process.env.S3_ACCESS_KEY_SECRET || !process.env.S3_REGION) {
+    console.error('Missing required environment variables');
+    return;
+  }
+
+  console.log('Received Job Id:', id);
+
+  const tempDir = `/tmp/${id}`;
+  const inputParams = { Bucket: 'chillwave-video-intake', Key: id };
 
   try {
-    const outputKey = `${OUTPUT_PREFIX}chunk-${chunkIndex}.ts`;
+    console.log('Initializing Transcoding...');
+    
+    fs.mkdirSync(tempDir);
 
-    await new Promise((resolve, reject) => {
-      FFmpeg({ source: Buffer.from(videoChunk, 'base64') })
-        .outputOptions([
-          '-c:v libx264',
-          '-crf 23',
-          '-preset veryfast',
-          '-vf scale=-2:360',
-          '-f mpegts',
-          '-codec:v libx264',
-          '-codec:a aac',
-          '-hls_time 6', // Set the segment duration to 6 seconds
-          '-hls_playlist_type vod',
-        ])
-        .output(Buffer.from(''))
-        .on('error', reject)
-        .on('end', resolve)
-        .run();
-    });
+    const readStream = s3.getObject(inputParams).createReadStream(); // create S3 readStream
 
-    await s3
-      .upload({
-        Bucket: BUCKET_NAME,
-        Key: outputKey,
-        Body: FFmpeg.output,
+    let totalTime;
+
+    ffmpeg(readStream)
+      .on('start', () => {
+        console.log(`Transcoding ${id} to 360p`);
       })
-      .promise();
-
-    return { statusCode: 200 };
+      .on('codecData', data => {
+        totalTime = parseInt(data.duration.replace(/:/g, ''));
+        console.log(`Codec data: ${JSON.stringify(data)}`);
+      })
+      .on('progress', progress => {
+        const time = parseInt(progress.timemark.replace(/:/g, ''));
+        const percent = Math.ceil((time / totalTime) * 100);
+        console.log(`Progress: ${percent}%`);
+      })
+      .on('stderr', stderrLine => {
+        console.log('Stderr output:', stderrLine);
+      })
+      .on('error', (err, stdout, stderr) => {
+        console.log('Stderr:', stderr);
+        console.error('Error:', err.message);
+      })
+      .on('end', async (err, stdout, stderr) => {
+        console.log('Transcoding finished.');
+        try {
+          const fileUploadPromises = fs.readdirSync(tempDir).map(file => {
+            const fileStream = fs.createReadStream(path.join(tempDir, file));
+            const params = {
+              Bucket: 'chillwave-video-egress',
+              Key: `${id}/${file}`,
+              Body: fileStream
+            };
+            console.log(`Uploading ${file} to S3`);
+            return s3.putObject(params).promise();
+          });
+          await Promise.all(fileUploadPromises); // upload output to S3
+          console.log('All files uploaded to S3.');
+        } catch (uploadError) {
+          console.error('Error uploading files:', uploadError.message);
+        } finally {
+          console.log(stdout);
+        }
+      })
+      .outputOptions([
+        '-vf scale=w=640:h=360',
+        '-c:a aac',
+        '-ar 48000',
+        '-b:a 96k',
+        '-c:v h264',
+        '-profile:v main',
+        '-crf 20',
+        '-g 48',
+        '-keyint_min 48',
+        '-sc_threshold 0',
+        '-b:v 800k',
+        '-maxrate 856k',
+        '-bufsize 1200k',
+        '-f hls',
+        '-hls_time 4',
+        '-hls_playlist_type vod',
+        `-hls_segment_filename ${path.join(tempDir, '360p_%d.ts')}`
+      ])
+      .output(path.join(tempDir, '360p.m3u8')) // output files are temporarily stored in tmp directory
+      .run();
   } catch (error) {
-    console.error(error);
-    return { statusCode: 500, body: error.toString() };
+    console.error('An error occurred:', error.message);
+  } finally {
+    // Clean up temporary files
+    if (fs.existsSync(tempDir)) {
+      fs.rmdirSync(tempDir, { recursive: true });
+      console.log('Temporary files deleted.');
+    }
   }
-};
+}
---
+++ apps/functions/transcode-480p/packages/sample/hello/hello.js
@@ -0,0 +1,188 @@
+const S3 = require('aws-sdk/clients/s3');
+const fs = require('fs');
+const path = require('path');
+const Ffmpeg = require('fluent-ffmpeg');
+const tar = require('tar');
+const { promisify } = require('util');
+const stream = require('stream');
+const lzma = require('lzma-native');
+const pipeline = promisify(stream.pipeline);
+
+const s3 = new S3({
+  s3ForcePathStyle: false,
+  endpoint: 'https://nyc3.digitaloceanspaces.com',
+  region: process.env.S3_REGION,
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_ACCESS_KEY_SECRET,
+  }
+});
+
+async function downloadFfmpeg(ffmpegKey, downloadPath) {
+  console.log('Downloading FFmpeg binary from S3...');
+  const params = { Bucket: 'chillwave', Key: ffmpegKey };
+  const ffmpegStream = s3.getObject(params).createReadStream();
+  const writeStream = fs.createWriteStream(downloadPath);
+  await pipeline(ffmpegStream, writeStream);
+  console.log('FFmpeg binary downloaded.');
+}
+
+async function decompressFfmpeg(archivePath, decompressedPath) {
+  console.log('Decompressing FFmpeg binary...');
+  return new Promise((resolve, reject) => {
+    const input = fs.createReadStream(archivePath);
+    const decompressor = lzma.createDecompressor();
+    const output = fs.createWriteStream(decompressedPath);
+
+    input.pipe(decompressor).pipe(output)
+      .on('finish', () => {
+        console.log('Decompression completed.');
+        resolve();
+      })
+      .on('error', reject);
+  });
+}
+
+async function extractFfmpeg(decompressedPath, extractPath) {
+  console.log('Extracting FFmpeg tar file...');
+  return new Promise((resolve, reject) => {
+    tar.x({
+        file: decompressedPath,
+        C: extractPath
+    }).then( () => {
+        console.log('Extracting FFmpeg tar file completed.');
+        resolve();
+    }).catch((err) => {
+        reject(err);
+    });
+  }) 
+}
+
+async function setExecutablePermissions(filePath) {
+  console.log(`Setting executable permissions for ${filePath}...`);
+  fs.chmodSync(filePath, '755');
+}
+
+exports.main = async (args) => {
+  const id = args.jobId;
+  const ffmpegKey = 'ffmpeg/ffmpeg-release-amd64-static.tar.xz'; // Update with your actual ffmpeg tar.xz key
+  const ffmpegArchivePath = '/tmp/ffmpeg.tar.xz';
+  const ffmpegDecompressedPath = '/tmp/ffmpeg.tar';
+  const ffmpegExtractPath = '/tmp/ffmpeg';
+
+  if (!process.env.S3_ACCESS_KEY_ID || !process.env.S3_ACCESS_KEY_SECRET || !process.env.S3_REGION) {
+    console.error('Missing required environment variables');
+    return;
+  }
+
+  console.log('Received Job Id:', id);
+
+  const tempDir = `/tmp/${id}`;
+  const inputParams = { Bucket: 'chillwave-video-intake', Key: id };
+
+  try {
+    console.log('Initializing Transcoding...');
+
+    // Ensure the ffmpeg binary is available
+    if (!fs.existsSync(path.join(ffmpegExtractPath, 'ffmpeg'))) {
+      console.log('Downloading and extracting FFmpeg binary...');
+      fs.mkdirSync(ffmpegExtractPath, { recursive: true });
+      await downloadFfmpeg(ffmpegKey, ffmpegArchivePath);
+      await decompressFfmpeg(ffmpegArchivePath, ffmpegDecompressedPath);
+      await extractFfmpeg(ffmpegDecompressedPath, ffmpegExtractPath);
+      console.log('FFmpeg download and extraction complete.')
+    }
+
+    const ffmpegPath = path.join(ffmpegExtractPath,'ffmpeg-7.0.1-amd64-static', 'ffmpeg');
+    const ffprobePath = path.join(ffmpegExtractPath, 'ffmpeg-7.0.1-amd64-static', 'ffprobe');
+
+    if (!fs.existsSync(ffmpegPath)) {
+      throw new Error(`FFmpeg binary not found at ${ffmpegPath}`);
+    }
+
+    if (!fs.existsSync(ffprobePath)) {
+      throw new Error(`FFprobe binary not found at ${ffprobePath}`);
+    }
+
+    // Set executable permissions
+    await setExecutablePermissions(ffmpegPath);
+    await setExecutablePermissions(ffprobePath);
+
+    // Configure fluent-ffmpeg to use the downloaded binaries
+    Ffmpeg.setFfmpegPath(ffmpegPath);
+    Ffmpeg.setFfprobePath(ffprobePath);
+
+    fs.mkdirSync(tempDir);
+
+    const readStream = s3.getObject(inputParams).createReadStream(); // create S3 readStream
+
+    return new Promise((resolve, reject) => {
+        Ffmpeg(readStream)
+        .on('start', () => {
+            console.log(`Transcoding ${id} to 360p`);
+        })
+        .on('stderr', stderrLine => {
+            console.log('Stderr output:', stderrLine);
+        })
+        .on('error', (err, stdout, stderr) => {
+            console.log('Stderr:', stderr);
+            console.error('Error:', err.message);
+            console.error('Stdout:', stdout);
+            reject(err);
+        })
+        .on('end', async () => {
+            console.log('Transcoding finished.');
+            try {
+                const fileUploadPromises = fs.readdirSync(tempDir).map(file => {
+                    const fileStream = fs.createReadStream(path.join(tempDir, file));
+                    const params = {
+                    Bucket: 'chillwave-video-egress',
+                    Key: `${id}/${file}`,
+                    Body: fileStream
+                    };
+                    console.log(`Uploading ${file} to S3`);
+                    return s3.putObject(params).promise();
+                });
+                await Promise.all(fileUploadPromises); // upload output to S3
+                console.log('All files uploaded to S3.');
+                resolve()
+            } catch (uploadError) {
+                console.error('Error uploading files:', uploadError.message);
+                reject(uploadError.message)
+            }
+        })
+        .outputOptions([
+            '-loglevel quiet',
+            '-preset ultrafast',
+            '-vf scale=w=640:h=360',
+            '-c:a aac',
+            '-ar 48000',
+            '-b:a 96k',
+            '-c:v h264',
+            '-profile:v main',
+            '-crf 25',
+            '-g 48',
+            '-keyint_min 48',
+            '-sc_threshold 0',
+            '-b:v 800k',
+            '-maxrate 856k',
+            '-bufsize 600k',
+            '-f hls',
+            '-hls_time 4',
+            '-hls_playlist_type vod',
+            `-hls_segment_filename ${path.join(tempDir, '360p_%d.ts')}`
+        ])
+        .output(path.join(tempDir, '360p.m3u8')) // output files are temporarily stored in tmp directory
+        .run();
+    });
+
+  } catch (error) {
+    console.error('An error occurred:', error.message);
+  } finally {
+    // Clean up temporary files
+    if (fs.existsSync(tempDir)) {
+      fs.rmdirSync(tempDir, { recursive: true });
+      console.log('Temporary files deleted.');
+    }
+  }
+}
