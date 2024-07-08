8b6da715216ca4860bbc5e6c6ebb4cf56d8f2faf
Casey Mershon <caseymershon@gmail.com>
2024-07-08 09:39:38 -0400

    add env validation

--- apps/transcoding-processor/src/app.module.ts
+++ apps/transcoding-processor/src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { validationSchemaForEnv } from './config/environment-variables';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: validationSchemaForEnv,
+    }),
+  ],
   providers: [],
 })
 export class AppModule {}
---
+++ apps/transcoding-processor/src/config/environment-variables.ts
@@ -0,0 +1,21 @@
+import * as Joi from 'joi';
+
+export interface EnvironmentVariables {
+  DATABASE_URL: string;
+  S3_ACCESS_KEY_ID: string;
+  S3_ACCESS_KEY_SECRET: string;
+  S3_REGION: string;
+  REDIS_HOST: string;
+  REDIS_PORT: number;
+  REDIS_PASSWORD: string;
+}
+
+export const validationSchemaForEnv = Joi.object<EnvironmentVariables, true>({
+  DATABASE_URL: Joi.string().required(),
+  S3_ACCESS_KEY_ID: Joi.string().required(),
+  S3_ACCESS_KEY_SECRET: Joi.string().required(),
+  S3_REGION: Joi.string().required(),
+  REDIS_HOST: Joi.string().required(),
+  REDIS_PORT: Joi.number().required(),
+  REDIS_PASSWORD: Joi.string().required(),
+});
