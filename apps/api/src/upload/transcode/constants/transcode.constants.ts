ae705faebda05fd1188346249a59d76936afe392
Casey Mershon <caseymershon@gmail.com>
2024-07-12 13:35:29 -0400

    trim unused modules

--- apps/api/src/upload/transcode/constants/transcode.constants.ts
+++
@@ -1,4 +0,0 @@
-export abstract class Constants {
-    static readonly SERVERLESS_URL = "https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web";
-    static readonly SERVERLESS_NAMESPACE = "fn-49ee38d8-0f3a-42b2-9959-74c188151641";
-}
